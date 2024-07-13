64bf299940d2a70e9e8c8634a3c036301e1ef95d
arbi1990 <arbi.jridi@gmail.com>
2024-07-13 11:38:09 +0100

    Notflix App

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/movies-app'),
+      dir: require('path').join(__dirname, './coverage/notflix'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'movies-app'`, () => {
+  it(`should have as title 'notflix'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('movies-app');
+    expect(app.title).toEqual('notflix');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('movies-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('notflix app is running!');
   });
 });
